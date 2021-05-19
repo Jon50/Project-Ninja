5a92803ec37c17cb7c4768e6d073c9867f2bf71b
Joao <isql550@gmail.com>
2021-05-19 18:35:57 +0200

    Plugins

---
+++ Project Ninja Source/Assets/Plugins/swipePackage/ExampleScripts/SwipeExample.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeExample : MonoBehaviour
+{
+    // ***** To use swipe controls, add the below line to your script and check for the boolean values for true as done in the Update method*****
+    public swipe swipeControls;
+    //***********************************************************************
+    public float moveSensitivity = 0;
+    Vector3 desiredPosition;
+    void Update()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        if (swipeControls.SwipeLeft)
+        {
+            desiredPosition += Vector3.left;                    // What to do when swiped left comes here
+        }
+        if (swipeControls.SwipeRight)
+        {
+            desiredPosition += Vector3.right;                   // What to do when swiped right comes here
+        }
+        if (swipeControls.SwipeUp)
+        {
+            desiredPosition += Vector3.forward;                 // What to do when swiped up comes here
+        }
+        if (swipeControls.SwipeDown)
+        {
+            desiredPosition += Vector3.back;                    // What to do when swiped down comes here
+        }
+        if (swipeControls.SwipeUpLeft)
+        {
+            desiredPosition += (Vector3.left + Vector3.forward);                   // What to do when swiped up left comes here
+        }
+        if (swipeControls.SwipeUpRight)
+        {
+            desiredPosition += (Vector3.right + Vector3.forward);                 // What to do when swiped up right comes here
+        }
+        if (swipeControls.SwipeDownLeft)
+        {
+            desiredPosition += ( Vector3.back + Vector3.left);                    // What to do when swiped down left comes here
+        }
+        if (swipeControls.SwipeDownRight)
+        {
+            desiredPosition += (Vector3.back + Vector3.right);                    // What to do when swiped down right comes here
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, moveSensitivity * Time.deltaTime);
+    }
+}
---
+++ Project Ninja Source/Assets/Plugins/swipePackage/ExampleScripts/swipeText.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class swipeText : MonoBehaviour
+{
+    public swipe swipeAction;
+    public Text txt;
+
+    void Update()
+    {
+        if (swipeAction.SwipeLeft)
+        {
+            txt.text = "Left Swipe!";                 // What to do when swiped left comes here
+        }
+        if (swipeAction.SwipeRight)
+        {
+            txt.text = "Right Swipe!";                // What to do when swiped right comes here
+        }
+        if (swipeAction.SwipeUp)
+        {
+            txt.text = "Up Swipe!";                   // What to do when swiped up comes here
+        }
+        if (swipeAction.SwipeDown)
+        {
+            txt.text = "Down Swipe!";                 // What to do when swiped down comes here
+        }
+        if (swipeAction.SwipeUpLeft)
+        {
+            txt.text = "Up Left Swipe!";              // What to do when swiped up left comes here
+        }
+        if (swipeAction.SwipeUpRight)
+        {
+            txt.text = "Up Right Swipe!";             // What to do when swiped up right comes here
+        }
+        if (swipeAction.SwipeDownLeft)
+        {
+            txt.text = "Down Left Swipe!";            // What to do when swiped down left comes here
+        }
+        if (swipeAction.SwipeDownRight)
+        {
+            txt.text = "Down Right Swipe!";           // What to do when swiped down right comes here
+        }
+        if (swipeAction.LeftClick)
+        {
+            txt.text = "Left Click!";                // What to do when mouse left clicked
+        }
+        if (swipeAction.DoubleLeftClick)
+        {
+            txt.text = "Double Left Click!";         // What to do when mouse double left clicked
+        }
+        if (swipeAction.RightClick)
+        {
+            txt.text = "Right Click!";                // What to do when mouse right clicked
+        }
+        if (swipeAction.DoubleRightClick)
+        {
+            txt.text = "Double Right Click!";         // What to do when mouse double right clicked
+        }
+        if (swipeAction.Tap)
+        {
+            txt.text = "Tap!";                        // What to do when tapped in mobile
+        }
+        if (swipeAction.DoubleTap)
+        {
+            txt.text = "Double Tap!";                 // What to do when double tapped in mobile
+        }
+    }
+}
